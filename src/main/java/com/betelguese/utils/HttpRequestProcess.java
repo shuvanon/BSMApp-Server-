85d76b843c8c3bbe69bfaac4a4a3e729b7f3d60b
BluePandora <bluepandora666@gmail.com>
2015-03-21 15:37:20 +0600

    hh

--- src/main/java/com/betelguese/utils/HttpRequestProcess.java
+++ src/main/java/com/betelguese/utils/HttpRequestProcess.java
@@ -111,7 +111,7 @@ public class HttpRequestProcess implements RequestName {
 				return requestService.transactionRequest(requestName, request);
 			} else if (request.equals(UPDATE_REQUEST)) {
 				Log.d(TAG, UPDATE_REQUEST
-						+ " is called by the client applicationn");
+						+ " iiiiiiiiiiiiis called by the client applicationn");
 				return requestService.updateRequest(requestName, request);
 
 			} else if (requestName.equals(REPORT_REQUEST)) {
